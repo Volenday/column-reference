84843ee30d98ee415fdc9e625f98fda9ddd9e99a
Gabriel Joshua Paet <gabriel.paet@volenday.com>
2021-06-08 10:06:25 +0800

    v3.1.9

--- filter.js
+++ filter.js
@@ -1,16 +1,33 @@
-import React, { memo, useState } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import { Checkbox, Input, List, Popover } from 'antd';
 import { FilterOutlined } from '@ant-design/icons';
+import { remove, uniq } from 'lodash';
 
 import { GetValue } from './utils';
 
 const Filter = ({ column, dropdown, id, options, setFilter }) => {
-	const [selected, setSelected] = useState([column.filterValue]);
+	const [selected, setSelected] = useState(typeof column.filterValue !== 'undefined' ? [column.filterValue] : []);
 	const [newOptions, setNewOptions] = useState(options);
 
+	useEffect(() => {
+		const prevSelected = JSON.parse(localStorage.getItem('prevSelected'));
+
+		if (prevSelected && prevSelected.length > 0) {
+			setSelected([...prevSelected, ...selected]);
+			localStorage.setItem('prevSelected', JSON.stringify(remove(uniq([...prevSelected, ...selected]), null)));
+		} else {
+			if (!selected.includes(undefined))
+				localStorage.setItem('prevSelected', JSON.stringify(remove(uniq([...selected]), null)));
+		}
+	}, []);
+
 	const selectItem = (columnId, filterValue) => {
-		if (selected.includes(filterValue)) setFilter(columnId, '');
-		else {
+		if (selected.includes(filterValue)) {
+			setFilter(
+				columnId,
+				selected.filter(d => d !== filterValue)
+			);
+		} else {
 			setFilter(columnId, filterValue);
 			setSelected([...selected, filterValue]);
 		}
@@ -30,15 +47,28 @@ const Filter = ({ column, dropdown, id, options, setFilter }) => {
 	const handleSearch = value => {
 		const { fields, separator } = dropdown;
 
-		if (value === '') setNewOptions(options);
-		else setNewOptions(newOptions.filter(d => fields.map(f => GetValue(f, d)).join(separator) === value));
+		if (value === '') return setNewOptions(options);
+
+		setNewOptions(
+			newOptions.filter(d =>
+				fields
+					.map(f => GetValue(f, d))
+					.join(separator)
+					.match(value, 'i')
+			)
+		);
 	};
 
 	const renderPopoverContent = () => {
 		return (
 			<div>
-				<Input.Search allowClear onSearch={handleSearch} placeholder="Search" />
-				<List dataSource={newOptions} renderItem={renderItem} style={{ height: 300, overflow: 'scroll' }} />
+				<Input.Search
+					allowClear
+					onKeyUp={e => handleSearch(e.target.value)}
+					onSearch={handleSearch}
+					placeholder="Search"
+				/>
+				<List dataSource={newOptions} renderItem={renderItem} style={{ height: 250, overflowY: 'scroll' }} />
 			</div>
 		);
 	};
