a2c0446cccaac4d31ea3965476f2814cd87672b2
Jerome Miranda <jerome.miranda@volenday.com>
2021-06-09 12:58:08 +0800

    Remove localstorage and clean up the whole logic

--- filter.js
+++ filter.js
@@ -1,5 +1,5 @@
-import React, { memo, useCallback, useEffect, useMemo, useState } from 'react';
-import { Button, Checkbox, Divider, Input, List, Menu, Popover } from 'antd';
+import React, { memo, useEffect, useState } from 'react';
+import { Button, Checkbox, Divider, Input, List, Popover } from 'antd';
 import { FilterFilled, FilterOutlined } from '@ant-design/icons';
 
 import { GetValue } from './utils';
@@ -9,121 +9,92 @@ const Filter = ({ column, dropdown, id, options, setFilter }) => {
 	const [newOptions, setNewOptions] = useState(options);
 	const [isPopoverVisible, setIsPopoverVisible] = useState(false);
 
-	const savedFilters = useMemo(() => JSON.parse(localStorage.getItem('savedFilters')), [localStorage]);
-	const selectedColumn = useMemo(() => localStorage.getItem('selectedColumn'), [localStorage]);
+	const { fields, separator } = dropdown;
 
 	useEffect(() => {
-		if (savedFilters && savedFilters.length > 0) setSelected(savedFilters);
-	}, []);
+		if (!!column.filterValue) setSelected(column.filterValue);
+	}, [JSON.stringify(column.filterValue)]);
 
-	const selectItem = useCallback(
-		filterValue => {
-			if (selected.includes(filterValue)) {
-				setSelected(selected.filter(d => d !== filterValue));
-				localStorage.setItem('savedFilters', JSON.stringify(selected.filter(d => d !== filterValue)));
-			} else {
-				setSelected([...selected, filterValue]);
-				localStorage.setItem('savedFilters', JSON.stringify([...selected, filterValue]));
-			}
-		},
-		[JSON.stringify(selected), setSelected]
-	);
+	const selectItem = value => {
+		if (selected.includes(value)) setSelected(selected.filter(d => d !== value));
+		else setSelected([...selected, value]);
+	};
 
-	const renderItem = useCallback(
-		item => {
-			const { fields, separator } = dropdown;
-
-			return (
-				<List.Item style={{ cursor: 'pointer' }}>
-					<Checkbox checked={selected.includes(item.Id)} onChange={() => selectItem(item.Id)} />{' '}
-					{fields.map(f => GetValue(f, item)).join(separator)}
-				</List.Item>
-			);
-		},
-		[JSON.stringify(selected), selectItem, GetValue]
-	);
-
-	const handleSearch = useCallback(
-		value => {
-			const { fields, separator } = dropdown;
-
-			if (value === '') return setNewOptions(options);
-
-			setNewOptions(
-				newOptions.filter(d =>
-					fields
-						.map(f => GetValue(f, d))
-						.join(separator)
-						.match(new RegExp(value, 'i'))
-				)
-			);
-		},
-		[JSON.stringify(newOptions), setNewOptions]
-	);
-
-	const filter = useCallback(() => {
-		if (selected.length > 0) selected.map(d => setFilter(id, d));
-		else {
-			setFilter(id, '');
-			localStorage.setItem('selectedColumn', null);
-		}
-	}, [JSON.stringify(selected), id, setFilter]);
-
-	const renderPopoverContent = useCallback(() => {
+	const renderItem = item => {
 		return (
-			<>
-				<Menu>
-					<Menu.ItemGroup title="Filter">
-						<Input.Search
-							allowClear
-							onKeyUp={e => handleSearch(e.target.value)}
-							onSearch={handleSearch}
-							placeholder="Search"
-						/>
-						<List
-							dataSource={newOptions}
-							renderItem={renderItem}
-							style={{ height: 250, overflowY: 'scroll' }}
-						/>
-					</Menu.ItemGroup>
-					<Menu.ItemGroup title="Column Settings">
-						<Menu.Item>
-							<Checkbox {...column.getToggleHiddenProps()}>Visible</Checkbox>
-						</Menu.Item>
-					</Menu.ItemGroup>
-				</Menu>
-				<Divider />
-				<div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-					<div
-						style={{
-							display: 'flex',
-							justifyContent: 'space-between',
-							width: '50%'
-						}}>
-						<Button onClick={closePopover} type="default">
-							Cancel
-						</Button>
-						<Button onClick={filter} type="primary">
-							OK
-						</Button>
-					</div>
-				</div>
-			</>
+			<List.Item style={{ cursor: 'pointer', padding: '5px 0px' }}>
+				<Checkbox checked={selected.includes(item.Id)} onChange={() => selectItem(item.Id)}>
+					{fields.map(f => GetValue(f, item)).join(separator)}
+				</Checkbox>
+			</List.Item>
 		);
-	}, [JSON.stringify(newOptions), column, handleSearch, renderItem, closePopover, filter]);
+	};
 
-	const openPopover = useCallback(() => {
-		localStorage.setItem('selectedColumn', id);
-		setIsPopoverVisible(true);
-	}, [setIsPopoverVisible]);
+	const renderCount = () => {
+		if (!column.filterValue) return null;
+		if (!Array.isArray(column.filterValue)) return null;
 
-	const closePopover = useCallback(() => {
-		setIsPopoverVisible(false);
-	}, [setIsPopoverVisible]);
+		if (column.filterValue.length === 0) return null;
+		return <span>({column.filterValue.length})</span>;
+	};
+
+	const handleSearch = value => {
+		if (value === '') return setNewOptions(options);
+
+		setNewOptions(
+			options.filter(d =>
+				fields
+					.map(f => GetValue(f, d))
+					.join(separator)
+					.match(new RegExp(value, 'i'))
+			)
+		);
+	};
+
+	const filter = () => setFilter(id, selected);
+
+	const renderPopoverContent = () => (
+		<>
+			<div>
+				<h4>Filter {renderCount()}</h4>
+				<Input.Search
+					allowClear
+					onKeyUp={e => handleSearch(e.target.value)}
+					onSearch={handleSearch}
+					placeholder="Search"
+				/>
+				<List dataSource={newOptions} renderItem={renderItem} style={{ height: 250, overflowY: 'scroll' }} />
+			</div>
+			<Divider />
+			<div>
+				<h4>Column Settings</h4>
+				<Checkbox {...column.getToggleHiddenProps()}>Visible</Checkbox>
+			</div>
+			<Divider />
+			<div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+				<div
+					style={{
+						display: 'flex',
+						justifyContent: 'space-between',
+						width: '50%'
+					}}>
+					<Button onClick={closePopover} type="default">
+						Cancel
+					</Button>
+					<Button onClick={filter} type="primary">
+						OK
+					</Button>
+				</div>
+			</div>
+		</>
+	);
+
+	const openPopover = () => setIsPopoverVisible(true);
+	const closePopover = () => setIsPopoverVisible(false);
 
 	return (
 		<Popover content={renderPopoverContent} trigger="click" visible={isPopoverVisible}>
-			{savedFilters && selectedColumn && savedFilters.length > 0 && id === selectedColumn ? (
+			{!!column.filterValue ? (
 				<FilterFilled onClick={openPopover} style={{ cursor: 'pointer' }} />
 			) : (
 				<FilterOutlined onClick={openPopover} style={{ cursor: 'pointer' }} />
