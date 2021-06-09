9f989931369fd522342dea2ee6607caab669c2ed
Jerome Miranda <jerome.miranda@volenday.com>
2021-06-09 13:54:33 +0800

    Added sorting in the new filter

--- filter.js
+++ filter.js
@@ -8,12 +8,19 @@ const Filter = ({ column, dropdown, id, options, setFilter }) => {
 	const [selected, setSelected] = useState([]);
 	const [newOptions, setNewOptions] = useState(options);
 	const [isPopoverVisible, setIsPopoverVisible] = useState(false);
+	const [sort, setSort] = useState('');
 
 	const { fields, separator } = dropdown;
+
+	const withFilterValue = column.filterValue ? (column.filterValue.length !== 0 ? true : false) : false;
 
 	useEffect(() => {
 		if (!!column.filterValue) setSelected(column.filterValue);
 	}, [JSON.stringify(column.filterValue)]);
+
+	useEffect(() => {
+		setSort(column.isSorted ? (column.isSortedDesc ? 'DESC' : 'ASC') : '');
+	}, [column.isSorted, column.isSortedDesc]);
 
 	const selectItem = value => {
 		if (selected.includes(value)) setSelected(selected.filter(d => d !== value));
@@ -51,50 +58,80 @@ const Filter = ({ column, dropdown, id, options, setFilter }) => {
 		);
 	};
 
-	const filter = () => setFilter(id, selected);
+	const onOk = () => {
+		setFilter(id, selected);
 
-	const renderPopoverContent = () => (
-		<>
-			<div>
-				<h4>Filter {renderCount()}</h4>
-				<Input.Search
-					allowClear
-					onKeyUp={e => handleSearch(e.target.value)}
-					onSearch={handleSearch}
-					placeholder="Search"
-				/>
-				<List dataSource={newOptions} renderItem={renderItem} style={{ height: 250, overflowY: 'scroll' }} />
-			</div>
-			<Divider />
-			<div>
-				<h4>Column Settings</h4>
-				<Checkbox {...column.getToggleHiddenProps()}>Visible</Checkbox>
-			</div>
-			<Divider />
-			<div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-				<div
-					style={{
-						display: 'flex',
-						justifyContent: 'space-between',
-						width: '50%'
-					}}>
-					<Button onClick={closePopover} type="default">
-						Cancel
+		if (sort) column.toggleSortBy(sort === 'ASC' ? true : sort === 'DESC' ? false : '');
+		else column.clearSortBy();
+	};
+
+	const renderPopoverContent = () => {
+		const a2zType = sort === 'ASC' ? 'primary' : 'default',
+			z2aType = sort === 'DESC' ? 'primary' : 'default';
+
+		return (
+			<>
+				<div>
+					<h4>Sort</h4>
+					<Button
+						onClick={() => (sort !== 'ASC' ? setSort('ASC') : setSort(''))}
+						type={a2zType}
+						style={{ width: '49%' }}>
+						A to Z
 					</Button>
-					<Button onClick={filter} type="primary">
-						OK
+					<Button
+						onClick={() => (sort !== 'DESC' ? setSort('DESC') : setSort(''))}
+						type={z2aType}
+						style={{ marginLeft: '2%', width: '49%' }}>
+						Z to A
 					</Button>
 				</div>
-			</div>
-		</>
-	);
+				<Divider />
+				<div>
+					<h4>Filter {renderCount()}</h4>
+					<Input.Search
+						allowClear
+						onKeyUp={e => handleSearch(e.target.value)}
+						onSearch={handleSearch}
+						placeholder="Search"
+					/>
+					<List
+						dataSource={newOptions}
+						renderItem={renderItem}
+						style={{ height: 250, overflowY: 'scroll' }}
+					/>
+				</div>
+				<Divider />
+				<div>
+					<h4>Column Settings</h4>
+					<Checkbox {...column.getToggleHiddenProps()}>Visible</Checkbox>
+				</div>
+				<Divider />
+				<div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+					<div
+						style={{
+							display: 'flex',
+							justifyContent: 'space-between',
+							width: '50%'
+						}}>
+						<Button onClick={closePopover} type="default">
+							Cancel
+						</Button>
+						<Button onClick={onOk} type="primary">
+							OK
+						</Button>
+					</div>
+				</div>
+			</>
+		);
+	};
 
 	const openPopover = () => setIsPopoverVisible(true);
 	const closePopover = () => setIsPopoverVisible(false);
 
 	return (
 		<Popover content={renderPopoverContent} trigger="click" visible={isPopoverVisible}>
-			{!!column.filterValue ? (
+			{withFilterValue ? (
 				<FilterFilled onClick={openPopover} style={{ cursor: 'pointer' }} />
 			) : (
 				<FilterOutlined onClick={openPopover} style={{ cursor: 'pointer' }} />
